49be3f28616bfac3df857591bf1ece42c7e2b5a2
finderski <finderski@gmail.com>
2023-09-21 13:13:28 -0400

    Start
    The first upload

---
+++ traveller/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TRAVELLER = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+TRAVELLER.abilities = {
+	str: "TRAVELLER.AbilityStr",
+	dex: "TRAVELLER.AbilityDex",
+	con: "TRAVELLER.AbilityCon",
+	int: "TRAVELLER.AbilityInt",
+	wis: "TRAVELLER.AbilityWis",
+	cha: "TRAVELLER.AbilityCha",
+};
+
+TRAVELLER.abilityAbbreviations = {
+	str: "TRAVELLER.AbilityStrAbbr",
+	dex: "TRAVELLER.AbilityDexAbbr",
+	con: "TRAVELLER.AbilityConAbbr",
+	int: "TRAVELLER.AbilityIntAbbr",
+	wis: "TRAVELLER.AbilityWisAbbr",
+	cha: "TRAVELLER.AbilityChaAbbr",
+};
